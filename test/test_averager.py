0f8ff0a202287a580958b34b5e33e66e56ca648a
Vladislav Morozov <vladislav.v.morozov@gmail.com>
2025-08-10 13:25:04 +0200

    Unify parametrized tests

--- test/test_averager.py
+++ test/test_averager.py
@@ -4,121 +4,77 @@ import pytest
 from unit_averaging import InlineFocusFunction, UnitAverager
 
 
+# Fixture for creating a focus function
+@pytest.fixture
+def identity_focus_function():
+    return InlineFocusFunction(lambda x: x, lambda x: 1)
+
+
+@pytest.fixture
+def first_coord_focus_function():
+    return InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0]))
+
+
+# Parametrize the test data
+test_data = [
+    # Test data for weight computation
+    ("individual", [0, 0], [1, 1], np.array([1, 0])),
+    ("mean_group", [0, 0], [1, 1], np.array([1 / 2, 1 / 2])),
+    # Test data for averaging
+    ("individual", [0.5, 0], [1, 1], 0.5),
+    ("mean_group", [0.5, 0], [1, 1], 0.25),
+    # Test data for array inputs
+    (
+        "individual",
+        [np.array([3, 2]), np.array([4, 5])],
+        [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
+        3,
+    ),
+    (
+        "mean_group",
+        [np.array([3, 2]), np.array([4, 5])],
+        [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
+        3.5,
+    ),
+    (
+        "individual",
+        np.array([np.array([3, 2]), np.array([4, 5])]),
+        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
+        3,
+    ),
+    (
+        "mean_group",
+        np.array([np.array([3, 2]), np.array([4, 5])]),
+        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
+        3.5,
+    ),
+]
+
+
 @pytest.mark.parametrize(
-    "weight_scheme, ind_estimates, ind_covar_ests, expected",
-    [
-        ("individual", [0, 0], [1, 1], np.array([1, 0])),
-        ("mean_group", [0, 0], [1, 1], np.array([1 / 2, 1 / 2])),
-    ],
+    "weight_scheme, ind_estimates, ind_covar_ests, expected", test_data
 )
-def test_weight_list_scalars(
+def test_unit_averager(
     weight_scheme,
     ind_estimates,
     ind_covar_ests,
     expected,
+    identity_focus_function,
+    first_coord_focus_function,
 ):
-    """Test correct weight computation from lists of scalars"""
-    # Create focus function
-    identity = InlineFocusFunction(
-        lambda x: x,
-        lambda x: 1,
+    # Choose the appropriate focus function based on the test data
+    focus_function = (
+        first_coord_focus_function
+        if isinstance(ind_estimates[0], np.ndarray)
+        else identity_focus_function
     )
+
     # Create and fit averager
-    ua = UnitAverager(identity, weight_scheme, ind_estimates, ind_covar_ests)
+    ua = UnitAverager(focus_function, weight_scheme, ind_estimates, ind_covar_ests)
     ua.fit(target_id=0)
-    assert np.array_equal(ua.weights_, expected)
 
-
-@pytest.mark.parametrize(
-    "weight_scheme, ind_estimates, ind_covar_ests, expected",
-    [
-        ("individual", [0.5, 0], [1, 1], 0.5),
-        ("mean_group", [0.5, 0], [1, 1], 0.25),
-    ],
-)
-def test_average_list_scalars(
-    weight_scheme,
-    ind_estimates,
-    ind_covar_ests,
-    expected,
-):
-    """Test correct averaging from lists of scalars"""
-    # Create focus function
-    identity = InlineFocusFunction(
-        lambda x: x,
-        lambda x: 1,
-    )
-    # Create and fit averager
-    ua = UnitAverager(identity, weight_scheme, ind_estimates, ind_covar_ests)
-    ua.fit(target_id=0)
-    assert ua.estimate_ == expected
-
-
-@pytest.mark.parametrize(
-    "weight_scheme, ind_estimates, ind_covar_ests, expected",
-    [
-        (
-            "individual",
-            [np.array([3, 2]), np.array([4, 5])],
-            [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
-            3,
-        ),
-        (
-            "mean_group",
-            [np.array([3, 2]), np.array([4, 5])],
-            [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
-            3.5,
-        ),
-    ],
-)
-def test_average_list_arrays(
-    weight_scheme,
-    ind_estimates,
-    ind_covar_ests,
-    expected,
-):
-    """Test correct averaging from lists of NumPy arrays"""
-    # Create focus function
-    first_coord = InlineFocusFunction(
-        lambda x: x[0],
-        lambda x: np.array([1, 0]),
-    )
-    # Create and fit averager
-    ua = UnitAverager(first_coord, weight_scheme, ind_estimates, ind_covar_ests)
-    ua.fit(target_id=0)
-    assert ua.estimate_ == expected
-
-
-@pytest.mark.parametrize(
-    "weight_scheme, ind_estimates, ind_covar_ests, expected",
-    [
-        (
-            "individual",
-            np.array([np.array([3, 2]), np.array([4, 5])]),
-            np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-            3,
-        ),
-        (
-            "mean_group",
-            np.array([np.array([3, 2]), np.array([4, 5])]),
-            np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-            3.5,
-        ),
-    ],
-)
-def test_average_array_arrays(
-    weight_scheme,
-    ind_estimates,
-    ind_covar_ests,
-    expected,
-):
-    """Test averaging with array of arrays as input"""
-    # Create focus function
-    first_coord = InlineFocusFunction(
-        lambda x: x[0],
-        lambda x: np.array([1, 0]),
-    )
-    # Create and fit averager
-    ua = UnitAverager(first_coord, weight_scheme, ind_estimates, ind_covar_ests)
-    ua.fit(target_id=0)
-    assert ua.estimate_ == expected
+    # Check if the test is for weights or averaging
+    if isinstance(expected, np.ndarray):
+        assert np.array_equal(ua.weights_, expected)
+    else:
+        assert ua.estimate_ == expected
