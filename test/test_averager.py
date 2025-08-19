6b6f34250458cfd76f6c3a8c5cb8022baab3e060
vladislav-morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-08-19 15:52:11 +0200

    Add elementary tests for new approach

--- test/test_averager.py
+++ test/test_averager.py
@@ -1,196 +1,110 @@
 import numpy as np
 import pytest
 
-from unit_averaging import InlineFocusFunction, UnitAverager
+from unit_averaging import (
+    InlineFocusFunction,
+    IndividualUnitAverager,
+    MeanGroupUnitAverager,
+)
 
-# Test data for testing various inputs
-test_data = [
+
+# Test data for IndividualUnitAverager
+individual_test_data = [
     # Inputs: lists of scalars
     (
         InlineFocusFunction(lambda x: x, lambda x: 1),
-        "individual",
         [0, 1],
-        [1, 1],
         np.array([1, 0]),
         0,
     ),
+    # Inputs: lists of numpy arrays
+    (
+        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
+        [np.array([3, 2]), np.array([4, 5])],
+        np.array([1, 0]),
+        3,
+    ),
+    # Inputs: arrays of numpy arrays
+    (
+        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
+        np.array([np.array([3, 2]), np.array([4, 5])]),
+        np.array([1, 0]),
+        3,
+    ),
+]
+
+individual_test_ids = [
+    "scalar_list_individual",
+    "array_list_individual",
+    "array_of_arrays_individual",
+]
+
+# Test data for MeanGroupUnitAverager
+mean_group_test_data = [
+    # Inputs: lists of scalars
     (
         InlineFocusFunction(lambda x: x, lambda x: 1),
-        "mean_group",
         [0, 1],
-        [1, 1],
         np.array([0.5, 0.5]),
         0.5,
     ),
     # Inputs: lists of numpy arrays
     (
         InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        "individual",
         [np.array([3, 2]), np.array([4, 5])],
-        [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
-        np.array([1, 0]),
-        3,
-    ),
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        "mean_group",
-        [np.array([3, 2]), np.array([4, 5])],
-        [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])],
         np.array([0.5, 0.5]),
         3.5,
     ),
     # Inputs: arrays of numpy arrays
     (
         InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        "individual",
         np.array([np.array([3, 2]), np.array([4, 5])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-        np.array([1, 0]),
-        3,
-    ),
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        "mean_group",
-        np.array([np.array([3, 2]), np.array([4, 5])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
         np.array([0.5, 0.5]),
         3.5,
     ),
 ]
-test_ids = [
-    "scalar_list_individual",
+
+mean_group_test_ids = [
     "scalar_list_mean_group",
-    "array_list_individual",
     "array_list_mean_group",
-    "array_of_arrays_individual",
     "array_of_arrays_mean_group",
 ]
 
 
 @pytest.mark.parametrize(
-    "focus_function, weight_scheme, ind_estimates, "
-    "ind_covar_ests, expected_weights, expected_estimate",
-    test_data,
-    ids=test_ids,
+    "focus_function, ind_estimates, expected_weights, expected_estimate",
+    individual_test_data,
+    ids=individual_test_ids,
 )
-def test_unit_averager_various_inputs(
+def test_individual_unit_averager(
     focus_function,
-    weight_scheme,
     ind_estimates,
-    ind_covar_ests,
     expected_weights,
     expected_estimate,
 ):
-    """Test the ability of UnitAverager to take various inputs."""
-    ua = UnitAverager(
-        focus_function,
-        weight_scheme,
-        ind_estimates,
-        ind_covar_ests,
-    )
+    """Test the IndividualUnitAverager with various inputs."""
+    ua = IndividualUnitAverager(focus_function, ind_estimates)
     ua.fit(target_id=0)
-
     # Check weights and estimates
     assert np.allclose(ua.weights_, expected_weights, rtol=1e-03) and np.allclose(
         ua.estimate_, expected_estimate, rtol=1e-03
     ), "Weights or estimate do not match expected values."
 
 
-# Test data for testing various inputs
-test_data = [
-    # Same unit data (expect equal weights)
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        np.array([np.array([1.0, 1]), np.array([1, 1])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-        np.array([0.5, 0.5]),
-        1,
-    ),
-    # Other unit has crazy variance (expect individual weights)
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        np.array([np.array([1.0, 1]), np.array([1, 1])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[10e10, 0], [0, 10e10]])]),
-        np.array([1, 0]),
-        1,
-    ),
-    # Other unit has crazy bias
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        np.array([np.array([1.0, 1]), np.array([10e10, 1])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-        np.array([1, 0]),
-        1,
-    ),
-    # Target has crazy variance, other is biased
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        np.array([np.array([1.0, 1]), np.array([2, 1])]),
-        np.array([np.array([[10e10, 0], [0, 1]]), np.array([[1, 0], [0, 1]])]),
-        np.array([0, 1]),
-        2,
-    ),
-    # Meaningful averaging with two units
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0])),
-        np.array([np.array([1.0, 1]), np.array([1, 1])]),
-        np.array([np.array([[1, 0], [0, 1]]), np.array([[2, 0], [0, 1]])]),
-        np.array([2 / 3, 1 / 3]),
-        1,
-    ),
-    # Meaningful averaging with three units
-    # Two useful units, one crazy one
-    # Ten identical units
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0, 0, 0, 0])),
-        np.ones((10, 5)),
-        np.array([np.eye(5)] * 10),
-        np.ones((10, 1)) / 10,
-        1,
-    ),
-    # Other nine units are crazy
-    (
-        InlineFocusFunction(lambda x: x[0], lambda x: np.array([1, 0, 0, 0, 0])),
-        np.ones((10, 5)),
-        np.stack([np.eye(5), *np.array([np.eye(5) * 10e10] * 9)]),
-        np.stack([np.array(1), *np.zeros(9)]),
-        1,
-    ),
-    # Nonlinear focus function
-]
-test_ids = [
-    "2 units: identical",
-    "2 units: non-target with crazy variance",
-    "2 units: non-target with crazy bias",
-    "2 units: target has crazy variance, other is biased",
-    "2 units: reasonable",
-    "10 units: identical",
-    "10 units: non-targets have crazy variance",
-]
-
-
 @pytest.mark.parametrize(
-    "focus_function, ind_estimates, "
-    "ind_covar_ests, expected_weights, expected_estimate",
-    test_data,
-    ids=test_ids,
+    "focus_function, ind_estimates, expected_weights, expected_estimate",
+    mean_group_test_data,
+    ids=mean_group_test_ids,
 )
-def test_unit_averager_optional_fixed_n(
+def test_mean_group_unit_averager(
     focus_function,
     ind_estimates,
-    ind_covar_ests,
     expected_weights,
     expected_estimate,
 ):
-    """Test fixed-N (agnostic) optimal weights in UnitAverager."""
-    ua = UnitAverager(
-        focus_function,
-        "optimal",
-        ind_estimates,
-        ind_covar_ests,
-    )
+    """Test the MeanGroupUnitAverager with various inputs."""
+    ua = MeanGroupUnitAverager(focus_function, ind_estimates)
     ua.fit(target_id=0)
-
     # Check weights and estimates
     assert np.allclose(ua.weights_, expected_weights, rtol=1e-03) and np.allclose(
         ua.estimate_, expected_estimate, rtol=1e-03
