7fa760c5283a64b1ff396c60f33b0a178da0be0f
vladislav-morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-08-19 16:01:11 +0200

    Add optimal averager

--- src/unit_averaging/averager.py
+++ src/unit_averaging/averager.py
@@ -1,5 +1,6 @@
-from abc import ABC, abstractmethod 
+from abc import ABC, abstractmethod
 
+import cvxpy as cp
 import numpy as np
 
 from unit_averaging.focus_function import FocusFunction
@@ -15,7 +16,7 @@ class BaseUnitAverager(ABC):
     ):
         self.focus_function = focus_function
         self.ind_estimates = np.array(ind_estimates)
-        self.weights = None
+        self.weights_ = None
         self.estimate_ = None
 
     def fit(self, target_id: int):
@@ -84,3 +85,148 @@ class MeanGroupUnitAverager(BaseUnitAverager):
         num_units = len(self.ind_estimates)
         weights = np.ones(num_units) / num_units
         self.weights_ = weights
+
+
+class OptimalUnitAverager(BaseUnitAverager):
+    """Optimal unit averaging weight scheme that minimizes the plug-in MSE."""
+
+    def __init__(
+        self,
+        focus_function: FocusFunction,
+        ind_estimates: list | np.ndarray,
+        ind_covar_ests: list | np.ndarray,
+        unrestricted_units_bool: np.ndarray | list | None = None,
+    ):
+        super().__init__(focus_function, ind_estimates)
+        self.ind_covar_ests = np.array(ind_covar_ests)
+        self.unrestricted_units_bool = np.array(unrestricted_units_bool)
+
+    def _compute_weights(self):
+        # Estimate gradient and ensure it is a 1D numpy array
+        gradient_estimate_target = self._clean_gradient(
+            self.focus_function.gradient(self.ind_estimates[self.target_id_])
+        )
+
+        # Construct the objective function
+        quad_term = self._build_mse_matrix(
+            self.target_id_,
+            self.ind_estimates,
+            self.ind_covar_ests,
+            gradient_estimate_target,
+        )
+        num_coords = quad_term.shape[0]
+        lin_term = np.zeros((num_coords, 1))
+
+        # Specify the constrains
+        ineq_lhs = -np.identity(num_coords)
+        ineq_rhs = np.zeros(num_coords)
+        eq_lhs = np.ones((1, num_coords))
+        eq_rhs = np.array([1.0])
+
+        # Minimize the MSE and return the optimal weights
+        weights = cp.Variable(num_coords)
+        prob = cp.Problem(
+            cp.Minimize(
+                (1 / 2) * cp.quad_form(weights, quad_term) + lin_term.T @ weights
+            ),
+            [
+                ineq_lhs @ weights <= ineq_rhs,
+                eq_lhs @ weights == eq_rhs,
+            ],
+        )
+        # TODO: Resolve raise_error warning after https://github.com/cvxpy/cvxpy/issues/2851
+        prob.solve()
+        if weights.value is None:
+            raise TypeError(
+                "Optimizer could not find a feasible solution, returned None."
+            )
+
+        self.weights_ = weights.value
+
+    def _clean_gradient(self, gradient) -> np.ndarray:
+        """Ensure the gradient is a 1D numpy array."""
+        gradient = np.array(gradient)
+        if gradient.ndim == 0:
+            gradient = np.expand_dims(gradient, axis=0)
+        return gradient
+
+    def _build_mse_matrix(
+        self,
+        target_id: int,
+        ind_estimates: np.ndarray,
+        ind_covar_ests: np.ndarray,
+        gradient_estimate_target: np.ndarray,
+        unrestr_units_bool: np.ndarray | None = None,
+    ) -> np.ndarray:
+        """Build the objective matrix for optimal-weight unit averaging.
+
+        This function accommodates both fixed-N and large-N approximations.
+
+        Args:
+            target_id (int): index of the target unit in the estimates arrays
+            ind_estimates (np.ndarray): An array of individual parameter estimates
+                (thetas in notation of docs and paper).
+            ind_covar_ests (np.ndarray): An array of covariance matrices for
+                individual parameter estimates.
+            gradient_estimate_target (np.ndarray): 1D NumPy array with the estimated
+                gradient of the focus function for the target unit
+            unrestr_units_bool (np.ndarray | None, optional): Boolean array indicating
+                unrestricted units. True means the corresponding unit is unrestricted.
+                If None, all units are unrestricted.
+
+        Returns:
+            np.ndarray: the estimated MSE matrix (psi or Q in notation of paper)
+        """
+
+        # Detect fixed-N vs. large-N mode. Unrestrict all units with fixed-N
+        if unrestr_units_bool is None:
+            unrestr_units_bool = np.full(len(ind_estimates), True)
+
+        # Compute the MSE matrix of unrestricted units
+        unrstrct_coefs = ind_estimates[unrestr_units_bool]
+        unrstrct_covar = ind_covar_ests[unrestr_units_bool]
+
+        # Fill MSE matrix element-by-element
+        psi = np.empty((len(unrstrct_coefs), len(unrstrct_coefs)), dtype="float64")
+        for i in range(len(unrstrct_coefs)):
+            for j in range(len(unrstrct_coefs)):
+                # Difference between estimates
+                coef_dif_i = unrstrct_coefs[i] - ind_estimates[target_id]
+                coef_dif_j = unrstrct_coefs[j] - ind_estimates[target_id]
+                psi_ij = np.outer(coef_dif_i, coef_dif_j)
+                # add covariance when appropriate
+                if i == j:
+                    psi_ij += unrstrct_covar[i]
+                # Multiply by gradient
+                psi_ij = gradient_estimate_target @ psi_ij @ gradient_estimate_target
+                # Set the corresponding element
+                psi[i, j] = psi_ij
+
+        # If in large-N regime, add an outer row and column of restricted units
+        if sum(unrestr_units_bool) < len(ind_estimates):
+            q = np.empty(
+                (len(unrstrct_coefs) + 1, len(unrstrct_coefs) + 1),
+                dtype="float64",
+            )
+            q[:-1, :-1] = psi
+            b = np.empty(len(unrstrct_coefs), dtype="float64")
+
+            # Fill out the elements of the b vector
+            mg = ind_estimates.mean()
+            for i in range(len(b)):
+                b_i = np.outer(
+                    unrstrct_coefs[i] - ind_estimates[target_id],
+                    ind_estimates[target_id] - mg,
+                )
+                q[i, -1] = -gradient_estimate_target @ b_i @ gradient_estimate_target
+                q[-1, i] = q[i, -1]
+
+            # Insert the last element
+            q[-1, -1] = np.power(
+                gradient_estimate_target @ (ind_estimates[target_id] - mg),
+                2,
+            )
+        else:
+            q = psi
+
+        return q
