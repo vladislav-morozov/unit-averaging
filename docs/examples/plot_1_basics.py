533fec2b9a3c0136133c3650ce4b0df4d5a16bb6
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-09-10 18:04:11 +0200

    Start work on first tutorial

--- docs/examples/plot_1_basics.py
+++ docs/examples/plot_1_basics.py
@@ -2,14 +2,82 @@ r"""
 Getting Started with Unit Averaging
 ===================================
 
-This is an example
+This is a worked example showcasing how unit averaging can integrated
+
+Goes from raw data, shows the necessary data construction, and showcases 
 """
+
+# %%
+# Introduction
+# ----------------------------------
+
+import pandas as pd
 
 from unit_averaging import InlineFocusFunction, OptimalUnitAverager
 
 # %%
 # We will crea
+# We will . Heterogeneous settings. Different mechanics
+# Contains data on the 150 employment agent
+
+
+# %%
+# Problem and Data
+# ----------------------------------
+# A
+
+
+german_data = pd.read_csv(
+    "data/tutorial_data.csv", parse_dates=True, index_col="period"
+)
+german_data.iloc[-4:, [0, 2, -1]]
+
+# %%
+# Constructing Individual Estimates
+# ----------------------------------
+#
+# First step is to prepare the data. Optimal unit averaging
+
+
+# %%
+# The data is difference to ensure stationarity. We will be forecasting changes 
+# in unemployment
+
+# %%
+# All unit averagers expect data in two of two forms: numpy array or dict.
+
+# %%
+# Our target
+
+# %%
+# Using Optimal Unit Averaging
+# ----------------------------------
 #
 
-print(2)
+# %%
+# Agnostic
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# 
+# Define focus function. Observe that that in this case using the actual values 
+# Hannover in 2019, but being able to use different coefficients
 
+
+# %%
+# Agnostic
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+
+
+# %%
+# Our target
+
+
+# %%
+# With Prior Restrictions
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+
+# %%
+# Visualizing the weights
+# ----------------------------------
+#
