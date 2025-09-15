56ec86fa473abd0ba9d9094bf41deca966c0c58a
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-09-15 11:34:11 +0200

    Switch focus to Frankfurt

--- docs/examples/plot_1_basics.py
+++ docs/examples/plot_1_basics.py
@@ -6,6 +6,8 @@ This is a worked example showcasing how unit averaging can integrated.
 
 Goes from raw data, shows the necessary data construction, and showcases things.
 
+It also gives an essential explanation
+
 
 This ``unit-averaging`` package is designed to accommodate a variety of approaches
 for estimating individual models and transforming their parameters.
@@ -41,21 +43,26 @@ from unit_averaging import InlineFocusFunction, OptimalUnitAverager
 # ^^^^^^^^^^^^^
 #
 # Suppose that it is December, 2019 and we want to optimally *forecast
-# the unemployment rate* in *Cologne* in the next month.
+# the unemployment rate* in *Frankfurt* in the next month.
 # At our disposal we have a panel (longitudinal) dataset of unemployment rates
-# for all the regions of Germany, Cologne included.
+# for all the regions of Germany, Frankfurt included.
 #
 # What is the key challenge to using all this data efficiently? The regions are
 # different in unseen ways (or *heterogeneous*): each of them has its own
 # unemployment dynamics, driven by differences in their economies,
 # populations, and laws. Throwing the data on all the regions into a single prediction
-# model usually may yield a very biased forecast for Cologne (or for any other
+# model usually may yield a very biased forecast for Frankfurt (or for any other
 # specific region).
 #
 # Unit averaging is ensemble method specifically designed for efficient estimation
-# of such unit-specific parameters (e.g. unemployment in Cologne) for situations
+# of such unit-specific parameters (e.g. unemployment in Frankfurt) for situations
 # when you have data on multiple units. It is applicable both with panel data,
 # as in this example, and in meta-analysis settings.
+#
+# Essentially, unit averaging computes a weighted linear combination of the
+# estimates of all the units. In optimal unit averaging, the weights are chosen
+# to optimally trade the increase in bias due to using data on non-Frankfurt
+# units with the decrease in variance due to using more data.
 
 
 # %%
@@ -125,15 +132,15 @@ print(regions[:10])
 # dynamics.
 #
 # Focus Function
-# ^^^^^^^^^
+# ^^^^^^^^^^^^^^
 #
 # With the model in hand, we need to express the target parameter (unemployment
-# for Cologne in 01.2020) as a function of the parameters of the unit-level
+# for Frankfurt in 01.2020) as a function of the parameters of the unit-level
 # models. Mathematically, the model implies the following forecast function:
 #
 # .. math::
 #
-#   \mu(c, \alpha, \beta) = c + \alpha U_{Cologne, 12.2019} + \beta U_{Germany, 12.2019}
+#   \mu(c, \alpha, \beta) = c + \alpha U_{Frankfurt, 12.2019} + \beta U_{Germany, 12.2019}
 #
 # The function :math:`\mu` is called a *focus function*: it defines how the parameters
 # of the underlying models map into the actual final parameter of interest.
@@ -151,19 +158,19 @@ print(regions[:10])
 # ``InlineFocusFunction``. The latter option is more flexible and requires
 # implementing the focus function and its gradient as methods.
 #
-# In our case, the focus function and its gradient are relatively simple, so we 
-# use an ``InlineFocusFunction``. We pass suitable lambda functions as the 
+# In our case, the focus function and its gradient are relatively simple, so we
+# use an ``InlineFocusFunction``. We pass suitable lambda functions as the
 # ``focus_function`` and ``gradient`` arguments:
 
 # Extract data on last month of target region
-target_data = german_data.loc["2019-12", ["Köln", "Deutschland"]].to_numpy().squeeze()
+target_data = german_data.loc["2019-12", ["Frankfurt", "Deutschland"]].to_numpy().squeeze()
 
 # Construct focus function
-forecast_cologne_jan_2020 = InlineFocusFunction(
+forecast_frankfurt_jan_2020 = InlineFocusFunction(
     focus_function=lambda coef: coef[0]
     + coef[1] * target_data[0]
     + coef[2] * target_data[1],
-    gradient=lambda coefs: np.array([1, target_data[0], target_data[1]]),
+    gradient=lambda coef: np.array([1, target_data[0], target_data[1]]),
 )
 
 
@@ -171,38 +178,41 @@ forecast_cologne_jan_2020 = InlineFocusFunction(
 # Estimating Unit Models
 # ^^^^^^^^^^^^^^^^^^^^^^^
 #
-# The second input 
-# Our model will look like
-# The data is difference to ensure stationarity. We will be forecasting changes
-# in unemployment
-
-german_data = german_data.diff()
-german_data["Germany_lag"] = german_data["Deutschland"].shift(1)
-german_data = german_data.iloc[2:,]
-
-# %%
-# The key data preparation step consists in preparing data.
+# The second key required input for all averagers is the information on the
+# estimated unit-level parameters (in this case the estimated
+# :math:`c_i, \alpha_i, \beta_i`). In case of ``OptimalUnitAverager`` we need to
+# supply both the individual estimates and the associated unit-level estimated
+# covariance matrices of estimators.
 #
+# All unit averagers accept data in two forms: as numpy arrays of arrays of estimates
+# (and covariances, when appropriate), and dicts of estimates and covariances.
+# Dicts are appropriate when the individual units have descriptive identifier,
+# as in our case.
 #
-# All unit averagers expect data in two of two forms: numpy array or dict.
-# Dicts are appropriate when the individual units have descriptive identifiers.
-#
-# This is our case.
+# We initialize empty dicts for individual estimates and covariances:
 
 ind_estimates = {}
 ind_covar_ests = {}
 
 # %%
-# Now estimate.
+# We now estimate the coefficients :math:`c_i, \alpha_i, \beta_i` unit-by-unit
+# by running a suitable autoregression with ``statsmodels``. As a minor technical
+# note, we estimate the equation in differences to ensure stationarity:
 
+# Difference data and create lag of Germany-wide rate
+german_data = german_data.diff()
+german_data["Germany_lag"] = german_data["Deutschland"].shift(1)
+german_data = german_data.iloc[2:,]
+
+# Iterate through regions
 for region in regions:
     # Extract data and add lags
-    ind_data = german_data.loc[:, [region, "Deutschland"]]
+    ind_data = german_data.loc[:, [region, "Germany_lag"]]
     # Run an ARx(1) model
     ar_results = (
-        AutoReg(ind_data.loc[:, region], 1, exog=ind_data["Deutschland"])
+        AutoReg(ind_data.loc[:, region], 1, exog=ind_data["Germany_lag"])
     ).fit(cov_type="HAC", cov_kwds={"maxlags": 5})
-    # Add to dictionary
+    # Add to dictionaries
     ind_estimates[region] = ar_results.params.to_numpy()
     ind_covar_ests[region] = ar_results.cov_params().to_numpy()
 
@@ -216,19 +226,30 @@ for region in regions:
 # %%
 # Defining and Fitting the Averager
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Fit
+# We are now in a position to create and fit our ``OptimalUnitAverager``. As
+# inputs, we pass our focus function, the individual-level estimated coefficients,
+# and the individual-level coefficient covariances:
 
 averager = OptimalUnitAverager(
-    focus_function=forecast_cologne_jan_2020,
+    focus_function=forecast_frankfurt_jan_2020,
     ind_estimates=ind_estimates,
     ind_covar_ests=ind_covar_ests,
 )
 
-# %%
-# Now fit by specifying target ID. If the things were passed as a dictionary,
-# then you need to pass the corresponding key
 
-averager.fit(target_id="Köln")
+# %%
+# To fit any averager in this package, we call the ``fit()`` method and supply
+# the ID of the target unit. If the ``ind_estimates`` is a dict, the target unit
+# is identified by the corresponding key. If ``ind_estimates`` is an array, one uses
+# the index of the unit. We are in the former case:
+
+averager.fit(target_id="Frankfurt")
+
+# %%
+# .. seealso:: We are using the ``OptimalUnitAverager`` in the conceptually simpler
+#              agnostic ("fixed-N") regime. ``OptimalUnitAverager`` can also make
+#              use of prior information ("large-N") regime. See
+#              :doc:`this page <../theory/theory>` for further info.
 
 # %%
 #
@@ -244,21 +265,38 @@ averager.fit(target_id="Köln")
 # Results
 # ----------------------------------
 #
-# Contains two things: estimated averaging weights and average
+# Calling ``fit()`` on averager object makes it compute the averaging weights
+# and use them to compute the averaging estimate for the specified focus parameter.
+# These values are stored in the ``weights`` and ``estimates`` attributes,
+# respectively. We now take a brief look at each of the two.
 #
 # Weights
 # ^^^^^^^^
 #
-# NumPy array
+# We start with the weights. The computed weights are stored as NumPy arrays in
+# the ``weights`` attribute:
 
-print(averager.weights_[:10].round(3))
+print(averager.weights[:10].round(3))
 
 # %%
-# Plot
+# These weights can be matched with the corresponding units by accessing the 
+# ``keys`` attribute, which stores the keys of the supplied units as a NumPy
+# array:
+
+print(averager.keys[:10])
+ 
+# %%
+# One may easily create a dictionary of weights by combining the two attributes:
 
 weight_dict = {}
-for key, val in zip(averager.keys, averager.weights_, strict=False):
+for key, val in zip(averager.keys, averager.weights, strict=True):
     weight_dict[key] = val
+
+
+# %%
+# These weights may then be further analyzed for patterns. For example, we may
+# plot 
+ 
 
 weight_df = pd.Series(weight_dict).reset_index()
 weight_df.columns = ["aab", "weights"]
@@ -270,8 +308,10 @@ fig, ax = plot_germany(
     vmin=-0.005,
 )
 
-
 # %%
+# This map shows how the averaging estimator assigns weights to improve the 
+# quality of the forecast for Frankfurt. As we can see, it 
+# 
 # Unit averaging can also be used to discover patterns in the data
 # Stuttgart
 
@@ -285,4 +325,5 @@ fig, ax = plot_germany(
 # ``IndividualUnitAverager``
 
 # %%
-# Running ``average()`` with no inputs reuses the focus function passed to the constructor
+# Running ``average()`` with no inputs reuses the focus function passed to the 
+# constructor
